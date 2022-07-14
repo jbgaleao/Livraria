4087edc514661bb53a8993f49b10a97cd3598d89
Joao Galeao <jbgaleao@gmail.com>
2022-07-14 02:40:31 -0300

    implementado o metodo POST para Cliente

--- Livraria.WEB/Controllers/ClienteController.cs
+++ Livraria.WEB/Controllers/ClienteController.cs
@@ -21,5 +21,17 @@ namespace Livraria.WEB.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            oClienteService.oRepositoryCliente.Incluir(cliente);
+            return RedirectToAction("Index");
+        }
+
     }
 }
